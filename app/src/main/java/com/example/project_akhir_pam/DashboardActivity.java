505a925b285e5bf1612c48f9cdec136f70c66ddd
marcelkelvinn <marcelkelvin75@student.ub.ac.id>
2023-06-08 02:31:14 +0700

    Create DashboardActivity.java

---
+++ app/src/main/java/com/example/project_akhir_pam/DashboardActivity.java
@@ -0,0 +1,16 @@
+package com.example.project_akhir_pam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.Button;
+
+public class DashboardActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+
+
+    }
+}
